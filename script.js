f9247809175fd16fea4575eaf4e8e3219d1b1cbd
Carlos Leal Pineda <carlealp@outlook.com>
2024-09-25 17:14:47 -0500

    Inicio del Proyecto
    1. Se crearon los archivos.
    2. Se estableció la estructura principal que tendrá la página.
    3. Se hicieron los estilos iniciales de la página.
    4. Se estableció el formato de escritura y el formato de comentarios.
    5. No se realizó ningún script.

---
+++ script.js
@@ -0,0 +1,6 @@
+/* Titulo del Proyecto: pagina-pokemon.
+Elaborado por: Carlos Leal.
+Fecha de inicio: 24/09/24
+Version: 1.0 
+Fecha de inicio de esta version: 24/09/24
+Fecha de finalización de esta version: 25/09/24 */
